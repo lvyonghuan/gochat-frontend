86ab6e907282bed75934af939a1863f33cadf87d
Lv Yonghuan <1914128278@qq.com>
2025-02-26 15:05:58 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import ElementPlus from 'element-plus'
+import axios from 'axios'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App);
